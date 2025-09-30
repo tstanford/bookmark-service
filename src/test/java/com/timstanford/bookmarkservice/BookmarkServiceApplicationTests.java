0b99b487141c4a8c8c2e78b556ad4f4408d5a081
tim stanford <tstanford@gmail.com>
2025-09-30 18:50:20 +0100

    Fix failing test due to no local postgres

--- src/test/java/com/timstanford/bookmarkservice/BookmarkServiceApplicationTests.java
+++ src/test/java/com/timstanford/bookmarkservice/BookmarkServiceApplicationTests.java
@@ -1,15 +1,38 @@
 package com.timstanford.bookmarkservice;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class BookmarkServiceApplicationTests {
 
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+			"postgres:15-alpine"
+	);
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
+
+	@BeforeAll
+	static void beforeAll() {
+		postgres.start();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		postgres.stop();
+	}
+
 	@Test
 	void contextLoads() {
 	}
-
-
-
 }
