9817060abc015e415833b947c96e98919c8f7cbe
tim stanford <tstanford@gmail.com>
2025-10-28 11:42:56 +0000

    Add getter and setter

--- src/main/java/com/timstanford/bookmarkservice/data/User.java
+++ src/main/java/com/timstanford/bookmarkservice/data/User.java
@@ -3,4 +3,11 @@ package com.timstanford.bookmarkservice.data;
 public class User {
     private String username;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
