912406892979adb50ea234c48616839863c6ea5d
tim stanford <tstanford@gmail.com>
2025-10-29 19:30:59 +0000

    Add CORS annotation to register and login. Update bruno tests

--- src/main/java/com/timstanford/bookmarkservice/api/AuthenticationController.java
+++ src/main/java/com/timstanford/bookmarkservice/api/AuthenticationController.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
