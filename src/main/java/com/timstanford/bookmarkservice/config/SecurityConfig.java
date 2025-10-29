f028e34c7a9be888cbbc9dc6f8c3dd5803863171
tim stanford <tstanford@gmail.com>
2025-10-29 20:12:26 +0000

    Add CORS to config

--- src/main/java/com/timstanford/bookmarkservice/config/SecurityConfig.java
+++ src/main/java/com/timstanford/bookmarkservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.timstanford.bookmarkservice.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -35,6 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for REST APIs
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session
