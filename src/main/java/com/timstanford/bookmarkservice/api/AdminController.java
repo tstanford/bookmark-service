a08da094cc6279f7d4c07b4ecfae5478336b9eee
tim stanford <tstanford@gmail.com>
2025-11-01 12:39:25 +0000

    Add endpoints for role based security, changes endpoints

--- src/main/java/com/timstanford/bookmarkservice/api/AdminController.java
+++ src/main/java/com/timstanford/bookmarkservice/api/AdminController.java
@@ -2,19 +2,16 @@ package com.timstanford.bookmarkservice.api;
 
 import com.timstanford.bookmarkservice.data.User;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-
-@PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/api/admin")
+@CrossOrigin(origins = "*")
 public interface AdminController {
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users")
     List<User> getUsers();
-
-    @PreAuthorize("hasRole('USER')")
-    @GetMapping("/amiadmin")
-    Boolean amiAdmin();
 }
