1f04c8a010f16206c9d99d2337d864c681647065
tim stanford <tstanford@gmail.com>
2025-11-07 00:22:44 +0000

    Fix broken integration tests and added register and log and authorization headers to all protects endpoints

--- src/test/java/com/timstanford/bookmarkservice/BookmarksControllerITest.java
+++ src/test/java/com/timstanford/bookmarkservice/BookmarksControllerITest.java
@@ -1,178 +1,234 @@
-//package com.timstanford.bookmarkservice;
-//
-//import com.fasterxml.jackson.core.type.TypeReference;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.timstanford.bookmarkservice.data.Bookmark;
-//import com.timstanford.bookmarkservice.data.BookmarksRepository;
-//import com.timstanford.bookmarkservice.data.GroupRepository;
-//import com.timstanford.bookmarkservice.service.BookmarkService;
-//import com.timstanford.bookmarkservice.service.FaviconDownloader;
-//import com.timstanford.bookmarkservice.service.GroupResponse;
-//import org.junit.jupiter.api.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.DynamicPropertyRegistry;
-//import org.springframework.test.context.DynamicPropertySource;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.testcontainers.containers.PostgreSQLContainer;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class BookmarksControllerITest {
-//
-//    @MockBean
-//    FaviconDownloader faviconDownloader;
-//
-//    @Autowired
-//    ObjectMapper objectMapper;
-//
-//    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-//            "postgres:16-alpine"
-//    );
-//
-//    @DynamicPropertySource
-//    static void setProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-//        registry.add("spring.datasource.username", postgres::getUsername);
-//        registry.add("spring.datasource.password", postgres::getPassword);
-//    }
-//
-//    @BeforeAll
-//    static void beforeAll() {
-//        postgres.start();
-//    }
-//
-//    @AfterEach
-//    public void afterEach(){
-//        deleteAllData();
-//    }
-//
-//    @AfterAll
-//    static void afterAll() {
-//        postgres.stop();
-//    }
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private BookmarkService service;
-//
-//    @Autowired
-//    private BookmarksRepository bookmarksRepository;
-//
-//    @Autowired
-//    private GroupRepository groupRepository;
-//
-//    @Test
-//    public void willDeleteAll() throws Exception {
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("one", "http://www.alpha.com", "Alpha Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("one", "http://www.bravo.com", "Bravo Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("two", "http://www.charlie.com", "Charlie Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("two", "http://www.delta.com", "Delta Site"));
-//
-//        mockMvc.perform(delete("/all"))
-//                .andExpect(status().isOk());
-//
-//        String contentAsString = mockMvc.perform(get("/bookmarks"))
-//                .andExpect(status().isOk())
-//                .andReturn()
-//                .getResponse()
-//                .getContentAsString();
-//
-//        assertEquals("[]", contentAsString);
-//
-//    }
-//
-//    @Test
-//    public void willGetAllBookmarks() throws Exception {
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("one", "http://www.alpha.com", "Alpha Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("one", "http://www.bravo.com", "Bravo Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("two", "http://www.charlie.com", "Charlie Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("two", "http://www.delta.com", "Delta Site"));
-//
-//        String contentAsString = mockMvc.perform(get("/bookmarks"))
-//                .andExpect(status().isOk())
-//                .andReturn()
-//                .getResponse()
-//                .getContentAsString();
-//
-//        List<GroupResponse> groups = objectMapper.readValue(contentAsString, new TypeReference<>() {});
-//
-//        assertEquals(2, groups.size());
-//        assertEquals("one", groups.get(0).getName());
-//        assertEquals(2, groups.get(0).getBookmarks().size());
-//        assertEquals("two", groups.get(1).getName());
-//        assertEquals(2, groups.get(1).getBookmarks().size());
-//    }
-//
-//    @Test
-//    public void importFromYamlFile() throws Exception {
-//        mockMvc.perform(post("/import")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("---\n" +
-//                                "groups:\n" +
-//                                "  - name: Group one\n" +
-//                                "    bookmarks:\n" +
-//                                "      - title: Cloudflare\n" +
-//                                "        url: https://www.cloudflare.com/\n" +
-//                                "      - title: Jenkins\n" +
-//                                "        url: https://alpha.com\n" +
-//                                "      - title: PVE\n" +
-//                                "        url: https://beta.com\n" +
-//                                "      - title: Nginx Proxy Manager - Admin\n" +
-//                                "        url: http://delta.com/\n" +
-//                                "      - title: Portainer\n" +
-//                                "        url: https://blah.com\n" +
-//                                "      - title: Wordpress\n" +
-//                                "        url: https://someplace.com/\n" +
-//                                "      - title: K3S Test\n" +
-//                                "        url: https://testing.com/\n" +
-//                                "\n" +
-//                                "  - name: Blogs\n" +
-//                                "    bookmarks:\n" +
-//                                "      - title: Baeldung\n" +
-//                                "        url: https://www.baeldung.com/\n"))
-//                .andExpect(status().isOk());
-//
-//        var groups = groupRepository.findAll();
-//        assertEquals(2, groups.size());
-//    }
-//
-//    private void deleteAllData(){
-//        bookmarksRepository.deleteAll();
-//        groupRepository.deleteAll();
-//    }
-//
-//    @Test
-//    void deleteBookmark() throws Exception {
-//        var response = service.addBookmark(BookmarkTestData.createBookmarkTestData("one", "http://www.alpha.com", "Alpha Site"));
-//        service.addBookmark(BookmarkTestData.createBookmarkTestData("one", "http://www.bravo.com", "Bravo Site"));
-//
-//        mockMvc.perform(delete("/bookmarks/"+response.getId()))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        String contentAsString = mockMvc.perform(get("/bookmarks"))
-//                .andExpect(status().isOk())
-//                .andReturn()
-//                .getResponse()
-//                .getContentAsString();
-//
-//        List<GroupResponse> groups = objectMapper.readValue(contentAsString, new TypeReference<>() {});
-//
-//        assertEquals(1, groups.size());
-//        assertEquals("one", groups.get(0).getName());
-//        assertEquals(1, groups.get(0).getBookmarks().size());
-//        assertEquals("http://www.bravo.com", groups.get(0).getBookmarks().get(0).getUrl());
-//    }
-//}
+package com.timstanford.bookmarkservice;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.timstanford.bookmarkservice.data.BookmarksRepository;
+import com.timstanford.bookmarkservice.data.GroupRepository;
+import com.timstanford.bookmarkservice.data.UserRepository;
+import com.timstanford.bookmarkservice.security.UserService;
+import com.timstanford.bookmarkservice.service.BookmarkService;
+import com.timstanford.bookmarkservice.service.FaviconDownloader;
+import com.timstanford.bookmarkservice.service.GroupResponse;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookmarksControllerITest {
+
+    static final String USERNAME = "tim";
+    static final String PASSWORD = "password";
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+    
+    @LocalServerPort
+    int portNumber; 
+    
+    @MockBean
+    FaviconDownloader faviconDownloader;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private BookmarkService service;
+
+    @Autowired
+    private BookmarksRepository bookmarksRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    private String token;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        deleteAllData();
+    }
+
+    @BeforeEach
+    public void beforeEach() throws Exception {
+        userService.registerUser(USERNAME, PASSWORD);
+
+        String payload = """
+                {
+                    "username": "%s",
+                    "password": "%s"
+                }
+                """.formatted(USERNAME, PASSWORD);
+
+        ResponseEntity<String> result = postString("/api/login", payload);
+
+        assertNotNull(result);
+        token = result.getBody();
+    }
+
+    private @Nullable ResponseEntity<String> postString(String url, String payload) {
+        WebClient webClient = WebClient.create("http://localhost:" + portNumber + url);
+        ResponseEntity<String> result = webClient.post()
+                .bodyValue(payload)
+                .header("Content-Type", "application/json")
+                .retrieve()
+                .toEntity(String.class)
+                .block();
+        return result;
+    }
+
+    private @Nullable ResponseEntity<String> postStringWithAuth(String url, String payload) {
+        WebClient webClient = WebClient.create("http://localhost:" + portNumber + url);
+        ResponseEntity<String> result = webClient.post()
+                .bodyValue(payload)
+                .header("Content-Type", "application/json")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer "+token)
+                .retrieve()
+                .toEntity(String.class)
+                .block();
+        return result;
+    }
+
+    private @Nullable ResponseEntity<String> getString(String url) {
+        WebClient webClient = WebClient.create("http://localhost:" + portNumber + url);
+        ResponseEntity<String> result = webClient.get()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer "+token)
+                .retrieve()
+                .toEntity(String.class)
+                .block();
+        return result;
+    }
+
+    private @Nullable ResponseEntity<String> delete(String url) {
+        WebClient webClient = WebClient.create("http://localhost:" + portNumber + url);
+        ResponseEntity<String> result = webClient.delete()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer "+token)
+                .retrieve()
+                .toEntity(String.class)
+                .block();
+        return result;
+    }
+
+    @Test
+    public void doNothing(){
+        System.out.println("hello");
+    }
+
+    private void addBookmark(String title, String url, String groupName){
+        var payload = """
+        {
+            "title": "%s",
+            "url": "%s",
+            "groupName": "%s"
+        }
+        """.formatted(title, url, groupName);
+
+        postStringWithAuth("/api/bookmarks", payload);
+    }
+
+    @Test
+    public void willDeleteAll() throws Exception {
+        addBookmark("Alpha Site", "http://www.alpha.com", "one");
+        addBookmark("Bravo Site", "http://www.bravo.com", "one");
+        addBookmark("Charlie Site", "http://www.charlie.com", "two");
+        addBookmark("Delta Site", "http://www.delta.com", "two");
+
+        var deleteResult = delete("/api/all");
+
+        assertTrue(deleteResult.getStatusCode().is2xxSuccessful());
+
+        ResponseEntity<String> getResults = getString("/api/bookmarks");
+        assertTrue(getResults.getStatusCode().is2xxSuccessful());
+        assertNotNull(getResults);
+
+        assertEquals("[]", getResults.getBody());
+
+    }
+
+    @Test
+    public void willGetAllBookmarks() throws Exception {
+        addBookmark("Alpha Site", "http://www.alpha.com", "one");
+        addBookmark("Bravo Site", "http://www.bravo.com", "one");
+        addBookmark("Charlie Site", "http://www.charlie.com", "two");
+        addBookmark("Delta Site", "http://www.delta.com", "two");
+
+        ResponseEntity<String> getResult = getString("/api/bookmarks");
+        assertTrue(getResult.getStatusCode().is2xxSuccessful());
+
+        List<GroupResponse> groups = objectMapper.readValue(getResult.getBody(), new TypeReference<>() {});
+
+        assertEquals(2, groups.size());
+        assertEquals("one", groups.get(0).getName());
+        assertEquals(2, groups.get(0).getBookmarks().size());
+        assertEquals("two", groups.get(1).getName());
+        assertEquals(2, groups.get(1).getBookmarks().size());
+    }
+
+    @Test
+    public void importFromYamlFile() throws Exception {
+        var importFile = new String(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("importfile.yml")).readAllBytes());
+
+        ResponseEntity<String> postResult = postStringWithAuth("/api/import", importFile);
+        assertTrue(postResult.getStatusCode().is2xxSuccessful());
+
+        var groups = groupRepository.findAll();
+        assertEquals(2, groups.size());
+    }
+
+    private void deleteAllData() {
+        bookmarksRepository.deleteAll();
+        groupRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void deleteBookmark() throws Exception {
+        addBookmark("Alpha Site", "http://www.alpha.com", "one");
+        var result = Objects.requireNonNull(getString("/api/bookmarks")).getBody();
+        List<GroupResponse> groups = objectMapper.readValue(result, new TypeReference<>() {});
+        var id = groups.get(0).getBookmarks().get(0).getId();
+
+        delete("/api/bookmarks/"+id);
+
+        var getResult2 = Objects.requireNonNull(getString("/api/bookmarks")).getBody();
+        List<GroupResponse> groups2 = objectMapper.readValue(getResult2, new TypeReference<>() {
+        });
+
+        assertEquals(1, groups2.size());
+        assertEquals("one", groups2.get(0).getName());
+        assertEquals(0, groups2.get(0).getBookmarks().size());
+    }
+}
