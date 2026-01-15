588429803f403f11b23e3d001676838864ca4f73
tim stanford <tstanford@gmail.com>
2026-01-15 22:09:50 +0000

    Add new get email endpoint

--- src/main/java/com/timstanford/bookmarkservice/api/AdminCheckController.java
+++ src/main/java/com/timstanford/bookmarkservice/api/AdminCheckController.java
@@ -1,9 +1,12 @@
 package com.timstanford.bookmarkservice.api;
 
+import com.timstanford.bookmarkservice.data.User;
+import com.timstanford.bookmarkservice.data.UserRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 @RestController
 public class AdminCheckController {
+    private final UserRepository userRepository;
+
+    public AdminCheckController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/amiadmin")
     public Boolean amiAdmin() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -21,5 +30,12 @@ public class AdminCheckController {
         return authentication.getAuthorities().stream()
                 .map(a -> (GrantedAuthority) a)
                 .anyMatch(ga -> ga.getAuthority().equalsIgnoreCase("ROLE_ADMIN"));
+    }
+
+    @GetMapping("/getemail")
+    public String getEmail() {
+        var username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsernameIgnoreCase(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+        return user.getEmailAddress();
     }
 }
