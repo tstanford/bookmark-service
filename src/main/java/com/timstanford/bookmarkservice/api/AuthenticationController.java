6096336eef7f0b070bf819e607bbedb68883bcf8
tim stanford <tstanford@gmail.com>
2026-02-08 12:15:15 +0000

    Add refresh token generation and validation

--- src/main/java/com/timstanford/bookmarkservice/api/AuthenticationController.java
+++ src/main/java/com/timstanford/bookmarkservice/api/AuthenticationController.java
@@ -3,10 +3,13 @@ package com.timstanford.bookmarkservice.api;
 import com.timstanford.bookmarkservice.security.JwtService;
 import com.timstanford.bookmarkservice.security.LoginRequest;
 import com.timstanford.bookmarkservice.security.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,11 +30,32 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-        String token = jwtService.generateToken(authentication);
-        return ResponseEntity.ok(token);
+        String authToken = jwtService.generateToken(authentication);
+        String refreshToken = jwtService.generateRefreshToken(authentication);
+
+        var response = new LoginResponse(authToken, refreshToken);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponse> refreshAuthToken(@RequestBody String refreshToken) {
+        var username = jwtService.extractUsername(refreshToken);
+        var user = userService.loadUserByUsername(username);
+
+        if(jwtService.isRefreshToken(refreshToken) &&
+                user.isAccountNonLocked() &&
+                !jwtService.isTokenExpired(refreshToken)){
+            Authentication auth = new UsernamePasswordAuthenticationToken(username, "");
+            String newAuthToken = jwtService.generateToken(auth);
+            String newRefreshToken = jwtService.generateRefreshToken(auth);
+            var response = new LoginResponse(newAuthToken, newRefreshToken);
+            return ResponseEntity.ok(response);
+        }
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
